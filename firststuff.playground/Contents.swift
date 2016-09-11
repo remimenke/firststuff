86e8da7fd1255bc9f94a05abcadc00595aae2ebf
remimenke <menke.remi@gmail.com>
2016-09-11 14:48:12 +0200

    first

---
+++ firststuff.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Mens {
+    var haar : UIColor
+    var ledematen : Int
+    var gewicht : Int
+    var iq : Int
+    var hoogte : Int
+    
+    func combatPower() -> Int {
+        return ledematen + gewicht * (iq - hoogte)
+    }
+}
+
+func main() {1
+    var haarKleur = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    var remi = Mens(haar: haarKleur, ledematen: 4, gewicht: 70, iq: 50, hoogte: 180)
+    
+    print(remi.combatPower())
+    
+    
+    
+//    var remi = Mens(haar: UIColor(red: 10/255, green: 0.5, blue: 0.3, alpha: 1), ledematen: 4)
+//    remi.haar
+//    remi.ledematen = remi.ledematen - 1
+//    remi.ledematen
+}
+
+main()
